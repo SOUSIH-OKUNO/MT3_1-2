b208fbad71de8485529f3485cd70bd39b088680e
k023g1075-okuno-soushi <k023g1075@g.neec.ac.jp>
2024-08-02 10:44:17 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -8,33 +8,32 @@ const char kWindowTitle[] = "GC2D_03_オクノ_ソウシ";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+	AABB aabb1{
+		.min{-0.5f, -0.5f, -0.5f},
+		.max{0.0f, 0.0f, 0.0f},
+	};
+
+	AABB aabb2{
+		.min{0.2f, 0.2f, 0.2f},
+		.max{1.0f, 1.0f, 1.0f},
+	};
+
+	int32_t color = WHITE;
+
 	Vector3 rotate = {};
 	Vector3 translate = {};
-	Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
-	Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
-	Vector3 kLocalVertices[3] = {
-		{0.0f, 0.5f, 0.0f,},
-		{0.5f, -0.5f, 0.0f},
-		{-0.5f, -0.5f, 0.0f}
-	};
+	Vector3 cameraRotate = { 0.0f, 0.0f, 0.0f };
+	Vector3 cameraTranslate = { 0.0f, 0.0f, -9.49f };
+	Vector3 cameraPosition = { 0,0,-5.0f };
 
-	Matrix4x4 worldMatrix = {};
-	Matrix4x4 cameraMatrix = {};
-	Matrix4x4 viewMatrix = {};
-	Matrix4x4 projectionMatrix = {};
-	Matrix4x4 worldViewProjectionMatrix = {};
+	Matrix4x4 viewProjectionMatrix = {};
 	Matrix4x4 viewportMatrix = {};
-
-	Sphere sphere = {
-		{0.0f, 0.0f, 0.0f},
-		0.5f
-	};
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -47,13 +46,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
-		worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate, translate);
-		cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
-		viewMatrix = Inverse(cameraMatrix);
-		projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		viewProjectionMatrix = MakeViewProjectionMatrix({ 1, 1, 1 }, rotate, translate, { 1, 1, 1 }, cameraRotate, cameraTranslate);
 		viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		if (isCollision(aabb1, aabb2)) {
+			color = RED;
+		}
+		else {
+			color = WHITE;
+		}
+
+
+		ImGui::DragFloat3("aabb1.min.", &aabb1.min.x, 0.01f);
+		ImGui::DragFloat3("aabb1.max.", &aabb1.max.x, 0.01f);
+		ImGui::DragFloat3("aabb2.min.", &aabb2.min.x, 0.01f);
+		ImGui::DragFloat3("aabb2.max.", &aabb2.max.x, 0.01f);
+		ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
+
+		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
+		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
+		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
+		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
+		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.max.z);
+		aabb1.max.z = (std::max)(aabb1.min.z, aabb1.max.z);
+		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.max.x);
+		aabb2.max.x = (std::max)(aabb2.min.x, aabb2.max.x);
+		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.max.y);
+		aabb2.max.y = (std::max)(aabb2.min.y, aabb2.max.y);
+		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.max.z);
+		aabb2.max.z = (std::max)(aabb2.min.z, aabb2.max.z);
 
 		///
 		/// ↑更新処理ここまで
@@ -62,16 +84,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
-		DrawSphere(sphere, worldViewProjectionMatrix, viewportMatrix, RED);
 
-		ImGui::Begin("Window");
-		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
-		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
-		ImGui::DragFloat3("sphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("sphereRadius", &sphere.radius, 0.01f);
-		ImGui::End();
-		///
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		DrawAABB(aabb1, viewProjectionMatrix, viewportMatrix, color);
+		DrawAABB(aabb2, viewProjectionMatrix, viewportMatrix, WHITE);
+
+		/// 
 		/// ↑描画処理ここまで
 		///
 
